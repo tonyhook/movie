42845aa76bec67c0dd2240cb27df6ac6b8fc9498
Tony Hook <tonyhook.su@gmail.com>
2018-02-25 18:33:19 +0800

    Fix timezone bug
    Fix a bug that jpa incorrectly saves releasedate in mysql

--- src/main/java/cc/tonyhook/movie/Application.java
+++ src/main/java/cc/tonyhook/movie/Application.java
@@ -1,5 +1,9 @@
 package cc.tonyhook.movie;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
